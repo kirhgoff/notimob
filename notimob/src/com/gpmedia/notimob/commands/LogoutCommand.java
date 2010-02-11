03df935c1ba9f33cd6b9050dc429e563a5c2de39
kirill.lastovirya <kirill.lastovirya@83d60f4a-13df-11df-8b42-5959a7990052>
2010-02-11 05:12:18 +0000

    login/logout functionality is working

---
+++ notimob/src/com/gpmedia/notimob/commands/LogoutCommand.java
@@ -0,0 +1,16 @@
+package com.gpmedia.notimob.commands;
+
+import java.util.Map;
+
+import com.gpmedia.notimob.systems.AuthSystem;
+
+public class LogoutCommand implements Command {
+
+	@Override
+	public void invoke(Map<String, Object> values, ParameterSource parameters) {
+		//TODO we need to clear all the session data here
+		AuthSystem.logOff ();
+		values.put("page", "login");
+	}
+
+}
